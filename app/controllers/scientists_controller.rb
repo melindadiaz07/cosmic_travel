95e69cf11a790c81ba843e92cb6b4620b9601d25
Melindadiaz07 <melindadiaz@me.com>
2020-09-22 15:21:20 -0400

    Built form_for in scientist new and create

--- app/controllers/scientists_controller.rb
+++ app/controllers/scientists_controller.rb
@@ -1,5 +1,6 @@
 class ScientistsController < ApplicationController
 
+    
     def index
         @scientists = Scientist.all
     end
@@ -7,5 +8,25 @@ class ScientistsController < ApplicationController
     def show
         @scientist = Scientist.find(params[:id])
     end
-    
+
+    def new
+        @scientist = Scientist.new
+    end
+
+    def create
+        scientist = Scientist.new(scientist_params(:name, :field_of_study))
+        # scientist.errors
+        scientist.save
+        redirect_to scientists_path
+    end
+
+
+    private
+
+    def scientist_params(*args)
+        params.require(:scientist).permit(*args)
+    end
+
+   
+
 end
--- app/models/scientist.rb
+++ app/models/scientist.rb
@@ -2,5 +2,14 @@ class Scientist < ApplicationRecord
     has_many :missions
     has_many :plants, through: :missions
 
-    
+    validates_uniqueness_of :name, message: " must be unique"
+
+
+    # private
+
+    # def unique_name?(try_name)
+    #     if Scientist.exists?(name: try_name) 
+    #         errors.add(:name, "Name must be unique")
+    #     end
+    # end
 end
